1d3b6400fb222ec88bbad5411a29c6215f80a9f3
Felipe Sardi <f@debie.redsi.co>
2026-02-02 06:40:58 -0500

    shell: sync function loader and gdmnt

--- .config/bash/rc.d/30-functions.sh
+++ .config/bash/rc.d/30-functions.sh
@@ -1,7 +1,4 @@
-# Load bash function libraries
-shopt -s nullglob
-for f in "$HOME/.config/bash/functions.d/"*.sh "$HOME/.config/bash/functions.d/"*.bash; do
-  source "$f"
+for f in "$HOME/.config/bash/functions.d/"*.sh; do
+  [[ -r "$f" ]] && source "$f"
 done
-shopt -u nullglob
 unset f
